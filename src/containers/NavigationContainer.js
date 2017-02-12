fbe7b6acfa671dd145ee4ad7b46a5e4651a83298
Kyeongmo Noh <kyeongmo2@gmail.com>
2017-02-12 19:08:15 +0900

    Update Navigation containers

--- src/containers/NavigationContainer.js
+++ src/containers/NavigationContainer.js
@@ -14,3 +14,26 @@ class NavigationContainer extends React.Component {
     );
   }
 }
+
+// GET STATE FROM STORE(REDUCERS) AND MATCH IT TO THIS.PROPS
+const mapStateToProps = (state) => {
+  return {
+    status: state.navigation.signin.status,
+  };
+};
+
+// NOT USED HERE YET
+// MATCH DISPATCH FUNCS OF ACTIONS TO THIS.PROPS
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signinRequest: (id, pw) => {
+      return dispatch(signinRequest(id, pw));
+    },
+    // signoutRequest: () => {
+    //   return dispatch(signoutRequest());
+    // },
+  };
+};
+
+export default connect(mapStateToProps,
+  mapDispatchToProps)(NavigationContainer);
