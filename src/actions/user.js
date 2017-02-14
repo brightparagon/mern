b8011b35fd18cd01945a957a00015f906d6b0e93
Kyeongmo Noh <kyeongmo2@gmail.com>
2017-02-14 17:21:19 +0900

    Update sign up actions & dispatcher in Redux

--- src/actions/user.js
+++ src/actions/user.js
@@ -12,9 +12,46 @@ import {
 } from './ActionTypes';
 import request from 'superagent';
 
-// Sign In
+// SIGN UP
+export function signupRequest(email, name, password) {
+  return (dispatch) => {
+    // set status
+    dispatch(signup());
+
+    return request
+      .post('/api/user/signup')
+      .send({email, name, password})
+      .end((err, res) => {
+        // --> Update codes in the server <error codes>
+        if(err) dispatch(signupFail(error.response.data.code));
+        dispatch(signupSuccess(email));
+      });
+  };
+}
+
+export function signup() {
+  return {
+    type: USER_SIGNUP,
+  };
+}
+
+export function signupSuccess() {
+  return {
+    type: USER_SIGNUP_SUCCESS,
+  };
+}
+
+export function signupFail(error) {
+  return {
+    type: AUTH_REGISTER_FAILURE,
+    error,
+  };
+}
+
+// SIGN IN
 export function signinRequest(email, password) {
   return (dispatch) => {
+    // set status
     dispatch(signin());
     return request
       .post('/api/user/signin')
