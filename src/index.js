3ecca123bec66a30cecf4011eca3ad63fcf2280e
Kyeongmo Noh <kyeongmo2@gmail.com>
2017-02-14 22:53:12 +0900

    Update router in react

--- src/index.js
+++ src/index.js
@@ -5,19 +5,19 @@ import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import reducers from './reducers';
 import thunk from 'redux-thunk';
-import {SignUp, SignIn} from './containers';
+import {SignUpContainer} from './containers';
 import App from './App';
 import './index.css';
 
 const store = createStore(reducers, applyMiddleware(thunk));
 const rootElement = document.getElementById('root');
+
 // ReactDOM.render(<App />, rootElement);
 ReactDOM.render(
   <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
-        <Route path="user/signup" component={SignUp}/>
-        <Route path="user/signin" component={SignIn}/>
+        <Route path="user/signup" component={SignUpContainer}/>
       </Route>
     </Router>
   </Provider>, rootElement
