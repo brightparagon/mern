714112c1ba40a4c32c087ddeedb73a73db2ab28b
Kyeongmo Noh <kyeongmo2@gmail.com>
2017-02-15 11:31:29 +0900

    Fix App in React about {this.props.children}

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React from 'react';
 import {NavigationContainer} from './containers';
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div>
         <NavigationContainer />
+        // 리액트 라우터 하위 항목들이 이 props.children으로 전달된다
+        {this.props.children}
       </div>
     );
   }
