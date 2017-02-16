9e287c5a779da10e51a8763bac270785c70c529e
Kyeongmo Noh <kyeongmo2@gmail.com>
2017-02-16 19:24:19 +0900

    Update sign up router in the server

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,34 +1,19 @@
 import express from 'express';
+import user from './user';
 // const jwt from 'express-jwt';
-import ctrlPost from './post';
-import ctrlUser from './user';
-import ctrlPage from './page';
-
-const router = express.Router();
-
 // const auth = jwt({
 //   secret: 'mernblog',
 //   userProperty: 'payload'
 // });
+const router = express.Router();
 
-// pages
-router.get('/upload', ctrlPage.upload);
-router.get('/signup', ctrlPage.signUp);
-router.get('/signin', ctrlPage.signIn);
-router.get('/', ctrlPage.index);
+router.use('/*', (req, res, next) => {
+  res.setHeader("Expires", "-1");
+  res.setHeader("Cache-Control", "must-revalidate, private");
+  next();
+});
 
-// data for post
-router.get('/list', ctrlPost.getPosts); // posts list in index.ejs
-router.post('/post', ctrlPost.createPost); // create a post
-router.get('/post', ctrlPost.getPost); // retrieve a post
-router.get('/updatePost', ctrlPost.getUpdatePost); // move to update page
-router.post('/postUpdate', ctrlPost.updatePost); // update a post
-router.get('/postDelete', ctrlPost.deletePost); // delete a post
-
-// data for user
-router.post('/user', ctrlUser.createUser); // create an user
-router.post('/signin', ctrlUser.signIn); // sign in
-router.get('/signout', ctrlUser.signOut); // sign out
-router.get('/profile', ctrlUser.profile); // get an user
+// DATA FOR USER
+router.use('/user', user);
 
 export default router;
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,23 +2,62 @@ import mongoose from 'mongoose';
 import passport from 'passport';
 import User from '../models/user';
 
-module.exports.createUser = function(req, res, next) {
-  const user = new User();
-  user.email = req.body.email;
-  user.name = req.body.name;
-  user.facebook = req.body.facebook;
-  user.setPassword(req.body.password);
-  user.save(function(err, user) {
-    if(err) return next(err);
-    // 여기서 로그인 유무 변수를 던져줘서 네비게이션을 통제
-    // var token;
-    // token = user.generateJwt();
-    const token = user.generateJwt();
-    // 토큰을 만들었지만 프론트 프레임워크가 없기 때문에 지금은 필요가 없다 -> 직접 user 전달
-    req.session.user = user;
-    res.render('profile', {user: req.session.user});
+/*
+    ACCOUNT SIGNUP: POST /api/user/signup
+    BODY SAMPLE: { "username": "test", "password": "test" }
+    ERROR CODES:
+        1: BAD USERNAME
+        2: BAD PASSWORD
+        3: USERNAM EXISTS
+*/
+router.post('/signup', (req, res) => {
+  // CHECK USERNAME FORMAT
+  // 정규식 for Email 만들기
+  // let usernameRegex = /^[a-z0-9]+$/;
+  //
+  // if(!usernameRegex.test(req.body.username)) {
+  //   return res.status(400).json({
+  //     error: "BAD USERNAME",
+  //     code: 1,
+  //   });
+  // }
+
+  // CHECK PASS LENGTH
+  if(req.body.password.length < 4 || typeof req.body.password !== "string") {
+    return res.status(400).json({
+      error: "BAD PASSWORD",
+      code: 2,
+    });
+  }
+
+  // CHECK USER EXISTANCE
+  User.findOne({email: req.body.email}, (err, exists) => {
+    if (err) throw err;
+    if(exists) {
+      return res.status(409).json({
+        error: "EMAIL EXISTS",
+        code: 3,
+      });
+    }
+
+    // CREATE ACCOUNT
+    let user = new User();
+    user.email = req.body.email;
+    user.name = req.body.name;
+    user.setPassword(req.body.password);
+
+    // SAVE IN THE DATABASE
+    user.save( (err, user) => {
+      if(err) throw err;
+      // 여기서 로그인 유무 변수를 던져줘서 네비게이션을 통제
+      const token = user.generateJwt();
+      // 토큰을 만들었지만 프론트 프레임워크가 없기 때문에 지금은 필요가 없다 -> 직접 user 전달
+      req.session.user = user;
+      return res.json({success: true});
+      // res.render('profile', {user: req.session.user});
+    });
   });
-};
+});
 
 module.exports.signIn = function(req, res, next) {
   passport.authenticate('local', function(err, user, info) {
